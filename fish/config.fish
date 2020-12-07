1b299baacbd53204273d2bc28176fb710e1083bf
KriegNL <68593093+KriegNL@users.noreply.github.com>
2020-12-07 09:34:05 +0100

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+
+
+abbr tree 'tree -C'
+abbr ls 'lsd'
+abbr l 'lsd -l'
+abbr neofetch 'neofetch --off'
+abbr editc 'micro ~/Git/frankenwm/config.h'
+abbr cdc 'cd ~/.config/'
+abbr cdh 'cd ~'
+abbr df 'df -h'
+
+set PATH $PATH /var/lib/snapd/snap/bin
